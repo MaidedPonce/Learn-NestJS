6503483ba4efb782b792f34ed85c9a80a2a93136
Maided Ponce <maided.h.p90@gmail.com>
2021-07-31 14:04:37 -0500

    Backend con NestJS

--- src/services/brand.service.ts
+++ src/services/brand.service.ts
@@ -1,16 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
 import { Brand } from '../entities/brand.entity';
 import { CreateBrandDto, UpdateBrandDto } from '../dtos/brand.dto';
-
 @Injectable()
 export class BrandsService {
   private counterId = 1;
   private brands: Brand[] = [
     {
       id: 1,
-      name: 'Brand 1',
-      image: 'https://i.imgur.com/U4iGx1j.jpeg',
+      name: 'M&M',
+      image: 'https://1000marcas.net/wp-content/uploads/2020/02/logo-MMs.png',
     },
   ];
 
@@ -19,39 +17,33 @@ export class BrandsService {
   }
 
   findOne(id: number) {
-    const product = this.brands.find((item) => item.id === id);
-    if (!product) {
-      throw new NotFoundException(`Brand #${id} not found`);
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) {
+      throw new NotFoundException(`Brand with ${id} doesn't exists`);
     }
-    return product;
+    return brand;
   }
 
-  create(data: CreateBrandDto) {
-    this.counterId = this.counterId + 1;
+  create(payload: CreateBrandDto) {
+    this.counterId = +1;
     const newBrand = {
       id: this.counterId,
-      ...data,
+      ...payload,
     };
-    this.brands.push(newBrand);
+
     return newBrand;
   }
 
   update(id: number, changes: UpdateBrandDto) {
     const brand = this.findOne(id);
+
     const index = this.brands.findIndex((item) => item.id === id);
+
     this.brands[index] = {
       ...brand,
       ...changes,
     };
     return this.brands[index];
   }
-
-  remove(id: number) {
-    const index = this.brands.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`Brand #${id} not found`);
-    }
-    this.brands.splice(index, 1);
-    return true;
-  }
+  
 }
